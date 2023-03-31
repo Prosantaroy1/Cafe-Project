8e7522078fbf756529943ee63d9468d3b528cd48
Prosantaroy1 <royprosanta172@gmail.com>
2023-03-31 17:00:52 +0600

    product design completed

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import './Product.css'
 const Product = (props) => {
     console.log(props.product);
-    const {name, tag, title, img} =props.product;
+    const {name, tag, title, img, date, logo, time} =props.product;
     return (
         <div className='product'>
             <img src={img} alt='thamble'/>
-            <p>{name}</p>
+            <div className='title-container'>
+                <div className='part-1'>
+                    <img className='logo-img' src={logo} alt='logo-img'/>
+                    <div className='title-name'>
+                        <h3>{name}</h3>
+                        <p>{date}</p>
+                    </div>
+                </div>
+                <div className='part-2'>
+                    <p>{time} min read</p>
+                </div>
+            </div>
+            <h2 className='title-description'>{title}</h2>
+            <p className='tag'>{tag}</p>
+            <a href='#' className='link'>Mark as read</a>
+            <hr className='border'/>
         </div>
     );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -13,7 +13,7 @@ const Shop = () => {
             <div className='products-container'>
             {
                     products.map(product => <Product
-                    
+                    key={product.id}
                     product={product}
                     ></Product>)
                 }
